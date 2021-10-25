2dc832811c97953793746cc7311339abf281ef68
rossdrew <thek1ng13@hotmail.com>
2021-10-25 11:44:15 +0100

    Adding debug versioning information to site

--- journal-ui/src/App.js
+++ journal-ui/src/App.js
@@ -9,13 +9,27 @@ class App extends Component {
         lastUpdated: "unknown"
     }
 
+    entries: React.RefObject<JournalEntries>;
+    entrycreation: React.RefObject<EntryCreationForm>;
+
+    constructor(props: any){
+        super(props);
+
+        this.child = React.createRef();
+    }
+
     render() {
-        return (
-            <div className="App-header">
-                <JournalEntries />
-                <EntryCreationForm />
+        return <div className="App-header">
+            <div className="debug">
+                React v{React.version}, JavaScript v{window.jsver}
             </div>
-        )
+
+            <JournalEntries key="entry_display" ref={this.entries} />
+            <EntryCreationForm key="entry_creation" ref={this.entrycreation} requestRefresh={() => {
+                this.entries.current?.refresh();
+            }}
+            />
+        </div>
     }
 }
 
