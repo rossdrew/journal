91e6b1ca1d1215f602a3241d851970dc7e2a47f0
rossdrew <thek1ng13@hotmail.com>
2021-10-25 16:17:39 +0100

    Ensuring and reversing order of entries
    Previously we
    - relied on returned order which isn't safe.
    - ordered top to bottom, it's just easier in terms of focus to reverse that for now

--- journal-ui/src/components/journal-entries.js
+++ journal-ui/src/components/journal-entries.js
@@ -24,12 +24,15 @@ class JournalEntries extends Component {
     }
 
     render() {
+        //Ordered by date, latest at top - why doesn't this work in the refresh method?
+        this.state.entries.sort((a, b) => b.creation - a.creation).reverse();
+
         return (
             <div>
-                <h1>Journal Entries</h1>
+                <sup className="discrete">Last Updated: {this.state.lastUpdated.toLocaleString()}</sup>
 
                 {this.state.entries.map((entry, index) => (
-                    <div className="card" key={index}>
+                    <div className="card" key={this.entryCardKeyPrefix+index}>
                         <div className="card-body" key={this.entryCardKeyPrefix+"body"+index}>
                             <h5 className="card-title" key={this.entryCardKeyPrefix+"title"+index}>entry {index}</h5>
                             <h6 className="card-subtitle mb-2 text-muted" key={this.entryCardKeyPrefix+"subtitle"+index}><i>{new Date(entry.creation).toLocaleString()}</i></h6>
@@ -37,8 +40,6 @@ class JournalEntries extends Component {
                         </div>
                     </div>
                 ))}
-
-                <sup className="discrete">Last Updated: {this.state.lastUpdated.toLocaleString()}</sup>
             </div>
         )
     }
