be3d32cb30e8a2ab95b9b8e9eea29d66b185baaf
rossdrew <thek1ng13@hotmail.com>
2021-12-23 14:56:12 +0000

    Adding unit tests
    While adding a new feature, a bug was flagged here that simple unit tests would have caught.

---
+++ src/test/java/com/rox/journal/EntriesQueryTest.java
@@ -0,0 +1,78 @@
+package com.rox.journal;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EntriesQueryTest {
+
+    @Test
+    public void testDefaultQuery(){
+        final EntriesQuery defaultQuery = EntriesQuery.all();
+
+        assertEquals("", defaultQuery.getBodyContains());
+        assertEquals(0, defaultQuery.getStart());
+        assertEquals(Optional.empty(), defaultQuery.getSize());
+    }
+
+    @Test
+    public void testQueryWithBodyFilter(){
+        final EntriesQuery defaultQuery = EntriesQuery
+                .all()
+                .whereBodyContains(Optional.of("Ross"));
+
+        assertEquals("Ross", defaultQuery.getBodyContains());
+        assertEquals(0, defaultQuery.getStart());
+        assertEquals(Optional.empty(), defaultQuery.getSize());
+    }
+
+    @Test
+    public void testQueryWithStartIndexDefined(){
+        final EntriesQuery defaultQuery = EntriesQuery
+                .all()
+                .startingAtIndex(Optional.of(23));
+
+        assertEquals("", defaultQuery.getBodyContains());
+        assertEquals(23, defaultQuery.getStart());
+        assertEquals(Optional.empty(), defaultQuery.getSize());
+    }
+
+    @Test
+    public void testQueryWithLimitDefined(){
+        final EntriesQuery defaultQuery = EntriesQuery
+                .all()
+                .limitedTo(Optional.of(101));
+
+        assertEquals("", defaultQuery.getBodyContains());
+        assertEquals(0, defaultQuery.getStart());
+        assertEquals(101, defaultQuery.getSize());
+    }
+
+    @Test
+    public void testQueryWithRangeDefined(){
+        final EntriesQuery defaultQuery = EntriesQuery
+                .all()
+                .startingAtIndex(Optional.of(10))
+                .limitedTo(Optional.of(101));
+
+        assertEquals("", defaultQuery.getBodyContains());
+        assertEquals(10, defaultQuery.getStart());
+        assertEquals(101, defaultQuery.getSize());
+    }
+
+    @Test
+    public void testQueryWithRangeAndBodyFilterDefined(){
+        final EntriesQuery defaultQuery = EntriesQuery
+                .all()
+                .whereBodyContains(Optional.of("Locke Lamora"))
+                .startingAtIndex(Optional.of(10))
+                .limitedTo(Optional.of(101));
+
+        assertEquals("Locke Lamora", defaultQuery.getBodyContains());
+        assertEquals(10, defaultQuery.getStart());
+        assertEquals(101, defaultQuery.getSize());
+    }
+
+}
