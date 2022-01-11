674bed473c753f3113b32724e0100511af988f15
rossdrew <thek1ng13@hotmail.com>
2022-01-11 15:20:49 +0000

    A task structure to play with

---
+++ src/main/java/com/rox/journal/entry/Task.java
@@ -0,0 +1,45 @@
+package com.rox.journal.entry;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task {
+    /** The relationship of a given {@link Task} to the entity holding it */
+    public enum TaskRelationship {
+        START,
+        COMPLETE,
+        LINK
+    }
+
+    private String id;
+    private Map<JournalEntry, TaskRelationship> relationships = Collections.emptyMap();
+    private JournalEntry linkedEntries;
+
+    public Task(final String id){
+        this.id = id;
+    }
+
+    private Task(final String id, final Map<JournalEntry, TaskRelationship> relationships){
+        this.id = id;
+        this.relationships = relationships;
+    }
+
+    public Task startedAt(final JournalEntry startEntry){
+        return new Task(id, mapWrap(startEntry, TaskRelationship.START));
+    }
+
+    public Task completeAsOf(final JournalEntry completeEntry){
+        return new Task(id, mapWrap(completeEntry, TaskRelationship.COMPLETE));
+    }
+
+    public Task linkedTo(final JournalEntry linkedEntry){
+        return new Task(id, mapWrap(linkedEntry, TaskRelationship.LINK));
+    }
+
+    private Map<JournalEntry, TaskRelationship> mapWrap(final JournalEntry entry, final TaskRelationship relationship){
+        final Map<JournalEntry, TaskRelationship> relationships = new HashMap<>();
+        relationships.put(entry, relationship);
+        return relationships;
+    }
+}
